8ff982c8260530acb477bcf6204f98a59cf72b9d
anu@@0003 <anuroop@metrictreelabs.com>
2023-03-29 19:14:02 +0530

    init

---
+++ src/sections/auth/login/LoginForm.js
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// @mui
+import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+// components
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import Iconify from '../../../components/iconify';
+
+// ----------------------------------------------------------------------
+
+const validationSchem = yup.object({
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string('Enter your password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+});
+
+export default function LoginForm() {
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: validationSchem,
+    onSubmit: (values) => {
+      CallApi();
+    },
+  });
+
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClick = () => {
+    // navigate('/dashboard', { replace: true });
+  };
+
+  const CallApi = () => {
+    
+  }
+
+  return (
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <Stack spacing={3}>
+          <TextField
+            name="email"
+            id="email"
+            label="Email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+
+          <TextField
+            name="password"
+            id="password"
+            label="Password"
+            type={showPassword ? 'text' : 'password'}
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                    <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Stack>
+    
+
+      <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
+        <Checkbox name="remember" label="Remember me" />
+        <Link variant="subtitle2" underline="hover">
+          Forgot password?
+        </Link>
+      </Stack>
+
+      <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
+        Login
+      </LoadingButton>
+      </form>
+    </>
+  );
+}
